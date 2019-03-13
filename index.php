7827c7bfe6e9e67902ed1aa449493ba0789fad73
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-13 22:10:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
 $email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("test@example.com", "Example User");
+$email->setFrom("st.dm.br.04@mail.ru", "komar1376");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("test@example.com", "Example User");
+$email->addTo("st.dm.br.04@mail.ru", "komar1376");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
